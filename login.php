e9bbf8be0fff5576edaa34f4752c0da6ea4b8029
golamsarwar08 <sarwartasin1896@gmail.com>
2022-09-27 13:19:17 +0600

    login final

--- login.php
+++ login.php
@@ -1,28 +1,27 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database_name = "logindb";
-
-$conn = mysqli_connect($servername,$username,$password,$database_name);
-
-if(!conn){
-    die("connection failed".mysqli_connect_error());
-
-}
-
-if(isset($_POST['save'])){
-
     $email= $_POST['email'];
     $password= $_POST['password'];
 
-    $sql_query = "INSERT INTO `login`(`email`, `password`) VALUES ('$email','$password')";
+    // Database Connection
+    $con = new mysqli("localhost","root","","logindb");
 
-    if(mysqli_query($conn,$sql_query)){
-        echo "Login Successfully";
+    if($con->connect_error){
+        die("Try Again : ".$con->connect_error );
+    } else {
+        $stmt = $con->prepare("select * from login where email = ?");
+        $stmt -> bind_param("s",$email);
+        $stmt -> execute();
+        $stmt_result = $stmt->get_result();
+        if($stmt_result->num_rows>0){
+            $data = $stmt_result->fetch_assoc();
+            if($data['password']===$password){
+                echo "Login successful";
+            }
+            else{
+                echo "Invalid Email Or Password";
+            }
+        } else{
+            echo "Invalid email or password";
+        }
     }
-    else{
-        echo "Error : ". $sql . "" . mysqli_error($conn);
-    }
-    mysqli_close($conn);
-}
+?>
